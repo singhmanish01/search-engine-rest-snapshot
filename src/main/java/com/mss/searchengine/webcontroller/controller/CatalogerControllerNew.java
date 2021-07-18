ddd2a69d37961d4e8216613f768d481b2cb2fe83
singhmanish01 <58860880+singhmanish01@users.noreply.github.com>
2021-07-18 17:45:48 +0530

    Update CatalogerControllerNew.java

--- src/main/java/com/mss/searchengine/webcontroller/controller/CatalogerControllerNew.java
+++ src/main/java/com/mss/searchengine/webcontroller/controller/CatalogerControllerNew.java
@@ -12,6 +12,7 @@ import com.mss.searchengine.service.CatalogerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
