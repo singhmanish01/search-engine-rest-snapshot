c028e1fbaa80746948f0ec9e111e1aa143bf5338
singhmanish01 <58860880+singhmanish01@users.noreply.github.com>
2021-07-22 20:54:21 +0530

    Update DocumentController.java

--- src/main/java/com/mss/searchengine/controller/DocumentController.java
+++ src/main/java/com/mss/searchengine/controller/DocumentController.java
@@ -28,7 +28,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class DocumentController {
