924f33d6cf16eb7c448185096603d53ee0f1c13c
singhmanish01 <58860880+singhmanish01@users.noreply.github.com>
2021-08-03 15:56:23 +0530

    Update DocumentController.java

--- src/main/java/com/mss/searchengine/controller/DocumentController.java
+++ src/main/java/com/mss/searchengine/controller/DocumentController.java
@@ -1,17 +1,19 @@
 package com.mss.searchengine.controller;
 
-import com.mss.searchengine.constants.Messages;
-import com.mss.searchengine.dto.DocumentDto;
-import com.mss.searchengine.dto.SearchResponse;
-import com.mss.searchengine.indexingandsearching.SearchIndex;
-import com.mss.searchengine.model.Document;
-import com.mss.searchengine.model.DocumentType;
-import com.mss.searchengine.model.Language;
-import com.mss.searchengine.service.CatalogerService;
-import com.mss.searchengine.service.DocumentService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.PathResource;
@@ -28,11 +30,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.*;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.ArrayList;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.mss.searchengine.constants.Messages;
+import com.mss.searchengine.dto.DocumentDto;
+import com.mss.searchengine.dto.SearchResponse;
+import com.mss.searchengine.indexingandsearching.SearchIndex;
+import com.mss.searchengine.model.Document;
+import com.mss.searchengine.model.DocumentType;
+import com.mss.searchengine.model.Language;
+import com.mss.searchengine.service.CatalogerService;
+import com.mss.searchengine.service.DocumentService;
 
 @Controller
 public class DocumentController {
@@ -74,7 +83,12 @@ public class DocumentController {
 
     @PostMapping("/add-document")
     public String addDocument(RedirectAttributes redirectAttributes, @RequestParam("file") MultipartFile file, @ModelAttribute("doc") DocumentDto doc) throws JsonProcessingException {
-        doc.setDocumentType(file.getContentType().split("/")[1]);
+    	
+    	String[] arr = file.getOriginalFilename().split("\\.");
+    	System.out.println(Arrays.toString(arr));
+    	String ext = arr[arr.length-1];
+    	System.out.println(ext);
+        doc.setDocumentType(ext);        
 //        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 //        System.out.println("user: " + ((UserDetails)auth.getPrincipal()).getUsername());
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
@@ -83,15 +97,19 @@ public class DocumentController {
         System.out.println("file: " + file.getOriginalFilename()+", type:" + file);
 
         DocumentDto documentDto = documentService.getJson(document,file);
-        documentService.addDocument(documentDto, file);
+        boolean plflag =  documentService.addDocument(documentDto, file);
+        if(!plflag)
+        	return "error-page";
 //        return messages.DocumentUploadedMsg;
         redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + file.getOriginalFilename() + "!");
         return "redirect:/view-documents";
     }
-
+    
     @GetMapping("/search")
     public String searching(@RequestParam String query, Model model) throws Exception{
-        System.out.println(query);
+    	if(query.trim().isEmpty())
+    		return "redirect:/";
+    	System.out.println(query);
         List<SearchResponse> result = searchIndex.search(query);
         model.addAttribute("result",result);
         return "search-result";
@@ -100,14 +118,17 @@ public class DocumentController {
     @GetMapping(value = "/download/document")
     public ResponseEntity<InputStreamResource> download(@RequestParam long documentId) throws Exception{
         Document document = documentService.findDocumentById(documentId);
+        File file = new File(document.getDocumentFilePath());
         PathResource pdfFile = new PathResource(document.getDocumentFilePath());
-        String filePath = document.getDocumentFilePath();        
+                
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.parseMediaType("application/pdf"));
+        headers.setContentType(MediaType.parseMediaType((new Tika()).detect(file)));
         headers.add("Access-Control-Allow-Origin", "*");
         headers.add("Access-Control-Allow-Methods", "GET, POST, PUT");
         headers.add("Access-Control-Allow-Headers", "Content-Type");
-//        headers.add("Content-Disposition", filePath.substring(39) );
+        System.out.println("filename: " + file.getName());
+        String CDispo = String.format("attachment; filename=\"%s\"", file.getName());
+        headers.add("Content-Disposition", CDispo);
         headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
         headers.add("Pragma", "no-cache");
         headers.add("Expires", "0");
@@ -139,5 +160,5 @@ public class DocumentController {
 //    public ResponseEntity<?> editDocument(long documentId){
 //
 //    }
-
+    
 }
